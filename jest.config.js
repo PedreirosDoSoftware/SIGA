de343b42cacf4b88e45d8b4fcbe3ab0a68f5da6e
PedreirosDoSoftware <pedreirosdosoftware10@gmail.com>
2025-11-07 15:27:54 -0300

    .

--- jest.config.js
+++ jest.config.js
@@ -2,29 +2,29 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-
+  
   // Arquivos que o Jest deve transformar com ts-jest
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-
+  
   // Ignorar transformações em node_modules, exceto algumas libs modernas se precisar
   transformIgnorePatterns: [
-    '/node_modules/(?!(@some-esm-lib)/)', // remova ou ajuste se não for usar libs ESM
+    '/node_modules/(?!(@some-esm-lib)/)',
   ],
-
+  
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-
+  
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-
+  
   testPathIgnorePatterns: [
     '<rootDir>/.next/',
     '<rootDir>/node_modules/',
   ],
-
+  
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!src/**/*.d.ts',
