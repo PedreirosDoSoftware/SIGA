f1aa6a13d7a79477797a8fc94eecc6df43206039
PedreirosDoSoftware <pedreirosdosoftware10@gmail.com>
2025-11-14 17:45:09 -0300

    Update aluno.tsx

--- app/admin/cadastrar-usuario/aluno/aluno.tsx
+++ app/admin/cadastrar-usuario/aluno/aluno.tsx
@@ -9,7 +9,7 @@ import { Label } from "@/components/ui/label"
 import { signUp } from "@/lib/supabase/auth"
 import { createAluno } from "@/lib/supabase/database"
 import { GraduationCap } from "lucide-react"
-
+//pagian teste
 export default function CadastroAlunoPage() {
   const [formData, setFormData] = useState({
     nome: "",
